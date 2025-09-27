c7cedb6d7002293e3d97b9bb75b9db1e2e8d83bc
Emmanuel A. Priesley <everythingemmmah@gmail.com>
2025-09-27 14:41:47 -0400

    New push

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
-import Header from "./components/header/Header";
+
 
 export default function Home() {
   return (
-    <div className="items-left px-20 pr-20 pt-10 gap-6">
+    <div className="items-left px-20 pr-20 pt-10 flex gap-6">
       <main>
-        <Header />
+       
 
-        <div className="max-w-[542px] ">
+        <div className="max-w-[542px] text-custom-gray-800">
           <h2 className="text-3xl font-medium pt-[64px] pb-6 line-height-[34px]">
             Exploring what happens <br />
-            when <span>designer</span> thinks like an <span>engineer</span>.
+            when <span className="text-custom-gray-400 ">designer</span> thinks
+            like an <span className="text-custom-gray-400 ">engineer</span>.
           </h2>
-          <p className="text-base">
-            Hi, I’m Emmanuel - A Curious human who design interfaces and build
-            digital things for a living. Sometimes with Framer, Sometimes with
-            Figma, always with vibes.
+          <p className="text-lg line-height-[22x] pb-4">
+            Hi, I’m Emmanuel - A{" "}
+            <span className="text-custom-gray-400 ">Curious human</span> who
+            design interfaces and build digital things for a living. Sometimes
+            with Framer, Sometimes with Figma, always with vibes.
           </p>
 
           <p>
