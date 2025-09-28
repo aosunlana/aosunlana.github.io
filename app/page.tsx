d10110cf0f9e49ae151888d289589a3b296e65f4
Emmanuel A. Priesley <everythingemmmah@gmail.com>
2025-09-27 22:49:38 -0400

    New push

--- app/page.tsx
+++ app/page.tsx
@@ -24,7 +24,7 @@ const item: Variants = {
 
 export default function Home() {
   return (
-    <div className="min-h-dvh flex flex-col items-start px-4 md:px-20 pt-[max(env(safe-area-inset-top),16px)] md:pt-4 text-custom-gray-900">
+    <div className="min-h-dvh flex flex-col items-start px-4 md:px-20 pt-[max(env(safe-area-inset-top))] md:pt-4 text-custom-gray-900">
       <div className="w-full">
         <Header />
       </div>
